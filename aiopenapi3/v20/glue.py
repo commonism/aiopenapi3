8162f9792aad864571d7890dd0dc4a5ed9908b5d
Markus Koetter <koetter@cispa.de>
2022-06-27 09:53:33 +0200

    security - better requirements error message

--- aiopenapi3/v20/glue.py
+++ aiopenapi3/v20/glue.py
@@ -67,7 +67,9 @@ class Request(RequestBase):
             options = " or ".join(
                 sorted(map(lambda x: f"{{{x}}}", [" and ".join(sorted(i.__root__.keys())) for i in security]))
             )
-            raise ValueError(f"No security requirement satisfied (accepts {options})  given {self.security}")
+            raise ValueError(
+                f"No security requirement satisfied (accepts {options} given {{{' and '.join(sorted(self.security.keys()))}}})"
+            )
 
     def _prepare_secschemes(self, scheme: str, value: List[str]):
         """
