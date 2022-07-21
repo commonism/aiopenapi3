04c724495daaf16df604eab329a6a559ff0ec837
Markus Koetter <koetter@cispa.de>
2022-07-21 13:10:39 +0200

    log - /tmp/aiopenapi3-debug.log causes permission problems
    … even if no debug log

--- aiopenapi3/log.py
+++ aiopenapi3/log.py
@@ -29,47 +29,54 @@ def init(force=False):
     """export AIOPENAPI3_LOGGING_HANDLERS=debug to get /tmp/aiopenapi3-debug.log"""
     handlers.extend(filter(lambda x: len(x), os.environ.get("AIOPENAPI3_LOGGING_HANDLERS", "").split(",")))
 
-    logging.config.dictConfig(
-        {
-            "version": 1,
-            "formatters": {
-                "notimestamp": {
-                    "class": "logging.Formatter",
-                    "format": "%(name)-9s %(levelname)-4s %(message)s",
-                },
-                "detailed": {
-                    "class": "logging.Formatter",
-                    "format": "%(asctime)s %(name)-9s %(levelname)-4s %(message)s",
-                },
-                "plain": {
-                    "class": "logging.Formatter",
-                    "format": "%(message)s",
-                },
+    config = {
+        "version": 1,
+        "formatters": {
+            "notimestamp": {
+                "class": "logging.Formatter",
+                "format": "%(name)-9s %(levelname)-4s %(message)s",
             },
-            "handlers": {
-                "console": {
-                    "class": "logging.StreamHandler",
-                    "level": "DEBUG",
-                    "formatter": "plain",
-                },
-                "syslog": {
-                    "class": "logging.handlers.SysLogHandler",
-                    "level": "DEBUG",
-                    "formatter": "notimestamp",
-                    "address": "/dev/log",
-                    "facility": "user",
-                },
-                "debug": {
-                    "class": "logging.handlers.WatchedFileHandler",
-                    "level": "DEBUG",
-                    "formatter": "detailed",
-                    "filename": "/tmp/aiopenapi3-debug.log",
-                },
+            "detailed": {
+                "class": "logging.Formatter",
+                "format": "%(asctime)s %(name)-9s %(levelname)-4s %(message)s",
             },
-            #            "root": {"level": "DEBUG", "handlers": handlers},
-            "loggers": {
-                "aiopenapi3": {"level": "DEBUG", "handlers": handlers},
-                "httpx": {"level": "DEBUG", "propagate": False, "handlers": handlers},
+            "plain": {
+                "class": "logging.Formatter",
+                "format": "%(message)s",
             },
-        }
-    )
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "DEBUG",
+                "formatter": "plain",
+            },
+            "syslog": {
+                "class": "logging.handlers.SysLogHandler",
+                "level": "DEBUG",
+                "formatter": "notimestamp",
+                "address": "/dev/log",
+                "facility": "user",
+            },
+            "debug": {
+                "class": "logging.handlers.WatchedFileHandler",
+                "level": "DEBUG",
+                "formatter": "detailed",
+                "filename": "/tmp/aiopenapi3-debug.log",
+            },
+        },
+        # "root": {"level": "DEBUG", "handlers": handlers},
+        "loggers": {
+            "aiopenapi3": {"level": "DEBUG", "handlers": handlers},
+            "httpx": {"level": "DEBUG", "propagate": False, "handlers": handlers},
+        },
+    }
+
+    # remove unused
+    for i in frozenset(config["handlers"].keys()) - frozenset(handlers):
+        del config["handlers"][i]
+
+    for i in frozenset(config["formatters"]) - frozenset(map(lambda x: x["formatter"], config["handlers"].values())):
+        del config["formatters"][i]
+
+    logging.config.dictConfig(config)
