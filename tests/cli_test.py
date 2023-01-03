e1556854dc476d9469a3f211f6e5fc652b30c978
Markus Koetter <koetter@cispa.de>
2023-01-03 10:58:09 +0100

    test - fix cli

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -5,10 +5,10 @@ import pytest
 from aiopenapi3.cli import main
 
 
-def test_cli():
+def test_validate_cli():
     with pytest.raises(TypeError):
-        main(shlex.split("tests/fixtures/schema-yaml-tags-invalid.yaml"))
+        main(shlex.split("validate tests/fixtures/schema-yaml-tags-invalid.yaml"))
 
-    main(shlex.split("-C -l -v tests/fixtures/schema-yaml-tags-invalid.yaml"))
+    main(shlex.split("-v validate -C -l tests/fixtures/schema-yaml-tags-invalid.yaml"))
 
-    main(shlex.split("-D tag:yaml.org,2002:timestamp -l -v tests/fixtures/schema-yaml-tags-invalid.yaml"))
+    main(shlex.split("-v validate -D tag:yaml.org,2002:timestamp -l tests/fixtures/schema-yaml-tags-invalid.yaml"))
