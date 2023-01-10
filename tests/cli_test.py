2b5d969ad0331963c68f2569fc38ee74f0a66816
Markus Koetter <koetter@cispa.de>
2023-01-10 10:45:53 +0100

    cli - rename -C to -Y

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -9,6 +9,6 @@ def test_validate_cli():
     with pytest.raises(TypeError):
         main(shlex.split("validate tests/fixtures/schema-yaml-tags-invalid.yaml"))
 
-    main(shlex.split("-v validate -C -l tests/fixtures/schema-yaml-tags-invalid.yaml"))
+    main(shlex.split("-v validate -Y -l tests/fixtures/schema-yaml-tags-invalid.yaml"))
 
     main(shlex.split("-v validate -D tag:yaml.org,2002:timestamp -l tests/fixtures/schema-yaml-tags-invalid.yaml"))
