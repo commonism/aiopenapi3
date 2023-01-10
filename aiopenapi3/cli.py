5d7c0b9bd39d28c8d37625db78699484e2a2e506
Markus Koetter <koetter@cispa.de>
2023-01-10 10:45:53 +0100

    cli - rename -C to -Y

--- aiopenapi3/cli.py
+++ aiopenapi3/cli.py
@@ -3,7 +3,7 @@ import datetime
 import sys
 import json
 import itertools
-
+from typing import List
 from pstats import SortKey
 import pstats
 import io
@@ -11,6 +11,7 @@ import importlib.util
 import cProfile
 import tracemalloc
 import linecache
+import logging
 
 import yaml
 import yarl
@@ -18,6 +19,8 @@ import httpx
 
 import aiopenapi3.plugin
 
+logg = logging.getLogger()
+logging.basicConfig()
 
 if sys.version_info >= (3, 9):
     from pathlib import Path
@@ -39,7 +42,7 @@ class DefaultLoader(yaml.SafeLoader):
 log = None
 
 
-def loader_prepare(args):
+def loader_prepare(args, session_factory):
     ylc = DefaultLoader
 
     if args.yaml_compatibility:
@@ -59,19 +62,17 @@ def loader_prepare(args):
     path = yarl.URL(args.input)
     if path.scheme in ["http", "https"]:
 
-        def session_factory(*args, **kwargs) -> httpx.Client:
-            return httpx.Client(*args, verify=False, **kwargs)
-
         loader = WebLoader(baseurl=path.with_path("/").with_query({}), yload=ylc, session_factory=session_factory)
     else:
-        loader = ChainLoader([RedirectLoader(Path(l).expanduser(), yload=ylc) for l in args.locations])
+        locations = args.locations or [Path(args.input).parent]
+        loader = ChainLoader(*[RedirectLoader(Path(l).expanduser()) for l in locations], yload=ylc)
 
     return loader, ylc
 
 
 def loader_args(cmd):
     cmd.add_argument(
-        "-C",
+        "-Y",
         "--yaml-compatibility",
         action="store_true",
         default=False,
@@ -87,22 +88,117 @@ def loader_args(cmd):
         help="disable this tag from the YAML loader",
     )
     cmd.add_argument("-l", "--yaml-list-tags", action="store_true", default=False, help="list tags")
+    return cmd
+
+
+def plugins_load(baseurl, plugins: List[str]) -> List[aiopenapi3.plugin.Plugin]:
+    """
+    load Plugins from python files
+    :param args:
+    :return:
+    """
+    r = []
+    for p in plugins:
+        file, _, cls = p.partition(":")
+        cls = cls.split(",")
+
+        spec = importlib.util.spec_from_file_location("extra", file)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        for c in cls:
+            plugin = getattr(module, c)
+            varnames = plugin.__init__.__code__.co_varnames
+            if len(varnames) == 1:
+                obj = plugin()
+            elif varnames == ("self", "url"):
+                obj = plugin(baseurl)
+            else:
+                raise TypeError("Can't __init__ plugin - unknown args")
+            r.append(obj)
+    return r
+
+
+def tm_display_top(snapshot, key_type="lineno", limit=10):
+    """
+    copied from
+    https://docs.python.org/3/library/tracemalloc.html
+    """
+    snapshot = snapshot.filter_traces(
+        (
+            tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+            #                tracemalloc.Filter(False, "<unknown>"),
+        )
+    )
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        print("#%s: %s:%s: %.1f KiB" % (index, frame.filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print("    %s" % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+
+def pr_display_top(pr):
+    s = io.StringIO()
+    sortby = SortKey.CUMULATIVE
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
+
+
+def schema_display_stats(api, duration):
+    operations = list(
+        itertools.chain.from_iterable(
+            map(
+                lambda x: list(filter(lambda x: x, [x.delete, x.get, x.head, x.options, x.patch, x.post, x.put])),
+                api.paths._paths.values(),
+            )
+        )
+    )
+    print(f"…  {duration} (processing time)")
+    print(f"… {len(operations)=}")
+    operations = list(filter(lambda x: x.operationId, operations))
+    print(f"… {len(operations)=} (with operationId)")
+
+    def schemaof(x):
+        if isinstance(api._root, aiopenapi3.v20.Root):
+            return x.definitions
+        else:
+            return x.components.schemas
+
+    ss = 0
+    for idx, (name, v) in enumerate(api._documents.items()):
+        ss += len(schemaof(v))
+        print(f"… {idx} {name}: {len(schemaof(v))}")
+    print(f"… {ss=}")
 
 
 def main(argv=None):
     global log
+    plugins = []
     parser = argparse.ArgumentParser("aiopenapi3", description="Swagger 2.0, OpenAPI 3.0, OpenAPI 3.1 validator")
     parser.add_argument("-v", "--verbose", action="store_true", default=False, help="be verbose")
     parser.add_argument("-p", "--profile", action="store_true", default=False)
     parser.add_argument("-t", "--tracemalloc", action="store_true", default=False)
+    parser.add_argument("-P", "--plugins", action="append")
     parser.add_argument("-L", "--locations", action="append")
+    parser.add_argument("-C", "--cache")
     sub = parser.add_subparsers()
 
     cmd = sub.add_parser("convert")
     cmd.add_argument("input")
     cmd.add_argument("output")
     cmd.add_argument("-f", "--format", choices=["yaml", "json"], default=None)
-    loader_args(cmd)
+    cmd = loader_args(cmd)
 
     def cmd_convert(args):
         output = Path(args.output)
@@ -122,14 +218,20 @@ def main(argv=None):
     cmd.set_defaults(func=cmd_convert)
 
     cmd = sub.add_parser("call")
+    cmd.add_argument("input")
     cmd.add_argument("operationId")
+    cmd.add_argument("-m", "--method")
+    cmd.add_argument("-b", "--base-url")
     cmd.add_argument("-a", "--authenticate")
     cmd.add_argument("-p", "--parameters")
     cmd.add_argument("-d", "--data")
     cmd.add_argument("-f", "--format")
+    cmd.add_argument("--disable-ssl-validation", action="store_true", default=False)
+    loader_args(cmd)
 
     def cmd_call(args):
-        loader, _ = loader_prepare(args)
+
+        loader, _ = loader_prepare(args, session_factory)
 
         def prepare_arg(value):
             if value:
@@ -137,7 +239,7 @@ def main(argv=None):
                     with Path(value[1:]).open("rt") as f:
                         data = json.load(f)
                 else:
-                    data = json.loads(args.authenticate)
+                    data = json.loads(value)
             else:
                 data = None
             return data
@@ -145,60 +247,73 @@ def main(argv=None):
         auth = prepare_arg(args.authenticate)
         parameters = prepare_arg(args.parameters)
         data = prepare_arg(args.data)
+        import jmespath
 
-        api = OpenAPI.load_file(args.name, yarl.URL(args.name), loader=loader)
+        if args.format:
+            expr = jmespath.compile(args.format)
+        else:
+            expr = None
+
+        if args.cache:
+            try:
+                cache = Path(args.cache)
+                api = OpenAPI.cache_load(cache, plugins, session_factory)
+            except FileNotFoundError:
+                api = OpenAPI.load_file(
+                    args.input, yarl.URL(args.input), loader=loader, plugins=plugins, session_factory=session_factory
+                )
+                api.cache_store(cache)
+        else:
+            api = OpenAPI.load_file(
+                args.input, yarl.URL(args.input), loader=loader, plugins=plugins, session_factory=session_factory
+            )
+
+        if args.base_url:
+            api._base_url = yarl.URL(args.base_url)
+
         if auth:
             api.authenticate(**auth)
 
-        req = api.createRequest(args.operationId)
-        headers, ret, response = req.request(parameters=parameters, data=data)
-        print(ret)
+        if args.method:
+            req = api.createRequest((args.operationId, args.method))
+        else:
+            req = api.createRequest(args.operationId)
+        try:
+            headers, ret, response = req.request(parameters=parameters, data=data)
+        except aiopenapi3.errors.ResponseSchemaError as e:
+            print(e.response.json())
+            print(e.response.headers)
+            return
+
+        obj = ret.dict(exclude_defaults=True)
+
+        if args.format:
+            obj = expr.search(obj)
+        print(json.dumps(obj, indent=2, sort_keys=True))
 
     cmd.set_defaults(func=cmd_call)
 
     cmd = sub.add_parser("validate")
-    loader_args(cmd)
+    cmd = loader_args(cmd)
     cmd.add_argument("input")
 
     def cmd_validate(args):
-        loader, ylc = loader_prepare(args)
+        loader, ylc = loader_prepare(args, session_factory)
 
         try:
             begin = datetime.datetime.now()
-            api = OpenAPI.load_file(args.input, yarl.URL(args.input), plugins=plugins, loader=loader)
+            try:
+                api = OpenAPI.load_file(args.input, yarl.URL(args.input), plugins=plugins, loader=loader)
+            except aiopenapi3.errors.ReferenceResolutionError as e0:
+                print(f"{e0} {e0.document} {e0.element}")
+                return
             end = datetime.datetime.now()
             duration = end - begin
         except ValueError as e:
-            print(e)
+            logg.exception(e)
         else:
             if args.verbose:
-                operations = list(
-                    itertools.chain.from_iterable(
-                        map(
-                            lambda x: list(
-                                filter(lambda x: x, [x.delete, x.get, x.head, x.options, x.patch, x.post, x.put])
-                            ),
-                            api.paths._paths.values(),
-                        )
-                    )
-                )
-                print(f"…  {duration} (processing time)")
-                print(f"… {len(operations)=}")
-                operations = list(filter(lambda x: x.operationId, operations))
-                print(f"… {len(operations)=} (with operationId)")
-
-                def schemaof(x):
-                    if isinstance(api._root, aiopenapi3.v20.Root):
-                        return x.definitions
-                    else:
-                        return x.components.schemas
-
-                ss = 0
-                for idx, (name, v) in enumerate(api._documents.items()):
-                    ss += len(schemaof(v))
-                    print(f"… {idx} {name}: {len(schemaof(v))}")
-                print(f"… {ss=}")
-
+                schema_display_stats(api, duration)
             print("OK")
 
     cmd.set_defaults(func=cmd_validate)
@@ -207,6 +322,8 @@ def main(argv=None):
         args = parser.parse_args(argv)
     else:
         args = parser.parse_args()
+
+    plugins = plugins_load(args.input, args.plugins or [])
 
     def log_(s):
         if args.verbose:
@@ -221,6 +338,9 @@ def main(argv=None):
     if args.tracemalloc:
         tracemalloc.start()
 
+    def session_factory(*args_, **kwargs) -> httpx.Client:
+        return httpx.Client(*args_, verify=args.disable_ssl_validation is False, **kwargs)
+
     if args.func:
         args.func(args)
     else:
@@ -229,37 +349,8 @@ def main(argv=None):
     if args.tracemalloc:
         tm = tracemalloc.take_snapshot()
         tracemalloc.stop()
-
-        def display_top(snapshot, key_type="lineno", limit=10):
-            snapshot = snapshot.filter_traces(
-                (
-                    tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-                    #                tracemalloc.Filter(False, "<unknown>"),
-                )
-            )
-            top_stats = snapshot.statistics(key_type)
-
-            print("Top %s lines" % limit)
-            for index, stat in enumerate(top_stats[:limit], 1):
-                frame = stat.traceback[0]
-                print("#%s: %s:%s: %.1f KiB" % (index, frame.filename, frame.lineno, stat.size / 1024))
-                line = linecache.getline(frame.filename, frame.lineno).strip()
-                if line:
-                    print("    %s" % line)
-
-            other = top_stats[limit:]
-            if other:
-                size = sum(stat.size for stat in other)
-                print("%s other: %.1f KiB" % (len(other), size / 1024))
-            total = sum(stat.size for stat in top_stats)
-            print("Total allocated size: %.1f KiB" % (total / 1024))
-
-        display_top(tm, limit=25)
+        tm_display_top(tm, limit=25)
 
     if args.profile:
         pr.disable()
-        s = io.StringIO()
-        sortby = SortKey.CUMULATIVE
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
+        pr_display_top(pr)
