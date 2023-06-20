b9c3d2e7fe54294d1c1edefb32a826b09d37abb0
Markus Kötter <koetter@cispa.de>
2023-06-20 08:38:32 +0200

    pydanticv2 - model_extra_private_getattr is gone

--- aiopenapi3/v30/general.py
+++ aiopenapi3/v30/general.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from pydantic import Field
-from pydantic._internal._model_construction import model_extra_private_getattr
+
 
 from ..base import ObjectExtended, ObjectBase, ReferenceBase
 
@@ -33,17 +33,14 @@ class Reference(ObjectBase, ReferenceBase):
         extra="ignore",  # """This object cannot be extended with additional properties and any properties added SHALL be ignored."""
     )
 
-    def l__getattr__(self, item):
+    def __getattr__(self, item):
         if item != "_target":
             return getattr(self._target, item)
         else:
-            return model_extra_private_getattr(self, "_target")
+            return super().__getattr__(item)
 
     def __setattr__(self, item, value):
         if item != "_target":
             setattr(self._target, item, value)
         else:
             super().__setattr__(item, value)
-
-
-Reference.__getattr__ = Reference.l__getattr__
