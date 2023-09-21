ea015fd92a59fde94388fac10cf51100ee849e61
Isaac Jessup <ibjessup@gmail.com>
2023-09-21 09:14:11 -0400

    request - index operator interface
    Small QOL update to the OperationIndex class that adds the __getitem__ method to enable access to operations via index operations (square brackets).

--- aiopenapi3/request.py
+++ aiopenapi3/request.py
@@ -361,6 +361,13 @@ class OperationIndex:
         else:
             raise KeyError(f"operationId {item} not found in tags or operations")
 
+    def __getitem__(self, item: Union[str, Tuple[str, str]]):
+        """
+        index operator interface
+        access operations by operationId or (path, method)
+        """
+        return getattr(self, item) if isinstance(item, str) else self._api.createRequest(item)
+
     def __iter__(self):
         return self.Iter(self._root, self._use_operation_tags)
 
