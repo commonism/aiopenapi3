commonism/aiopenapi3

commit 322ebd13dc37896e65366106abb0af9ab470d650

ruff - B020

src/aiopenapi3/model.py
@@ -547,37 +547,37 @@ class Model:  # (BaseModel):
                     _names = tuple(filter(lambda x: x, _names))
                 r = [Literal[_names]]  # type: ignore[assignment,list-item]
             else:
-                for _type in Model.types(schema) if not _type else [_type]:
-                    if _type in ("boolean", "integer", "number", "string"):
-                        oneOf = [i for i in getattr(schema, "oneOf", []) if _type in Model.types(i)]
-                        anyOf = [i for i in getattr(schema, "anyOf", []) if _type in Model.types(i)]
-                        allOf = [i for i in getattr(schema, "allOf", []) if _type in Model.types(i)]
+                for _t in Model.types(schema) if not _type else [_type]:
+                    if _t in ("boolean", "integer", "number", "string"):
+                        oneOf = [i for i in getattr(schema, "oneOf", []) if _t in Model.types(i)]
+                        anyOf = [i for i in getattr(schema, "anyOf", []) if _t in Model.types(i)]
+                        allOf = [i for i in getattr(schema, "allOf", []) if _t in Model.types(i)]
 
                         if not (anyOf or oneOf or allOf):
-                            v = class_from_schema(schema, _type)
+                            v = class_from_schema(schema, _t)
                             r.append(v)
                         else:
-                            v = [Model.createAnnotation(i, _type=_type) for i in oneOf]
+                            v = [Model.createAnnotation(i, _type=_t) for i in oneOf]
                             r.extend(v)
-                            v = [Model.createAnnotation(i, _type=_type) for i in anyOf]
+                            v = [Model.createAnnotation(i, _type=_t) for i in anyOf]
                             r.extend(v)
-                            v = [Model.createAnnotation(i, _type=_type) for i in allOf]
+                            v = [Model.createAnnotation(i, _type=_t) for i in allOf]
                             r.extend(v)
-                    elif _type == "array":
+                    elif _t == "array":
                         r.extend(
-                            list(
-                                Model.createAnnotation(i, _type=_type)
+                            [
+                                Model.createAnnotation(i, _type=_t)
                                 for i in getattr(schema, "oneOf", [])
-                                if Model.is_type(i, _type)
-                            )
+                                if Model.is_type(i, _t)
+                            ]
                         )
 
                         r.extend(
-                            list(
-                                Model.createAnnotation(i, _type=_type)
+                            [
+                                Model.createAnnotation(i, _type=_t)
                                 for i in getattr(schema, "anyOf", [])
-                                if Model.is_type(i, _type)
-                            )
+                                if Model.is_type(i, _t)
+                            ]
                         )
 
                         if isinstance(schema.items, list):
@@ -595,12 +595,12 @@ class Model:  # (BaseModel):
                         else:
                             raise TypeError(schema.items)
                         r.append(v)  # type: ignore[arg-type]
-                    elif _type == "object":
+                    elif _t == "object":
                         r.append(schema.get_type(fwdref=fwdref))
-                    elif _type == "null":
+                    elif _t == "null":
                         nullable = True
                     else:
-                        raise ValueError(_type)
+                        raise ValueError(_t)
 
             if len(r) == 1:
                 rr = r[0]
@@ -609,7 +609,7 @@ class Model:  # (BaseModel):
             else:
                 rr = None  # type: ignore[assignment]
             if nullable is True:
-                rr = Optional[rr]  # type: ignore[assignment]
+                rr = rr | None  # type: ignore[assignment]
         elif isinstance(schema, ReferenceBase):
             rr = Model.createAnnotation(schema._target, fwdref=True)
         else:

pyproject.toml
@@ -89,7 +89,7 @@ ignore = [
 #    "B009",
 #    "B015",
 #    "B018",
-    "B020",
+#    "B020",
     "BLE001",
     "C400",
     "C408",

commit 702c48167ef9a3868290cc32bc0cccee46db2ead

schema - property of type null
regression by UP045 in 322ebd13 - rr can be None

src/aiopenapi3/model.py
@@ -601,7 +601,7 @@ class Model:  # (BaseModel):
             else:
                 rr = None  # type: ignore[assignment]
             if nullable is True:
-                rr = rr | None  # type: ignore[assignment]
+                rr = Optional[rr]  # noqa: UP045 #type: ignore[assignment]
         elif isinstance(schema, ReferenceBase):
             rr = Model.createAnnotation(schema._target, fwdref=True)
         else:

tests/conftest.py
@@ -609,3 +609,8 @@ def with_paths_parameter_querystring():
 @pytest.fixture
 def with_schema_tags_v32():
     yield _get_parsed_yaml("schema-tags-v32.yaml")
+
+
+@pytest.fixture
+def with_schema_property_null():
+    yield _get_parsed_yaml("schema-property-null.yaml")

tests/fixtures/schema-property-null.yaml
@@ -1,0 +1,12 @@
+openapi: "3.1.0"
+info:
+  version: 1.0.0
+  title: enum test
+
+components:
+  schemas:
+    nullable:
+      properties:
+        token:
+          type: 'null'
+      type: object

tests/schema_test.py
@@ -819,3 +819,7 @@ def test_schema_title_name_collision(with_schema_title_name_collision):
 def test_schema_discriminated_union_extends(with_schema_discriminated_union_extends):
     # AssertionError: A.c0
     api = OpenAPI("/", with_schema_discriminated_union_extends)
+
+
+def test_schema_property_null(with_schema_property_null):
+    api = OpenAPI("/", with_schema_property_null)